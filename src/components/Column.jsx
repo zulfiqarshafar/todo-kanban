15552bd99242f4ebbc5a8af8777dbe3e9ae171e4
zulfiqarshafar <zul.shafar@gmail.com>
2024-04-23 01:27:19 +0800

    feat: update column content

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import Task from './Task'
+import plusIcon from '../assets/images/plus-circle.svg'
 import '../assets/css/Column.css'
 
-function Column({ columnClass, columnTitle, columnMonth }) {
+function Column({ columnClass, columnTitle, columnMonth, setIsOpenCreateEditTask }) {
   return (
     <section className={`column ${columnClass}`}>
       <div className='column-title'>{ columnTitle }</div>
       <div className='column-month'>{ columnMonth }</div>
       <Task />
-      <div>New Task</div>
+      <div className='new-task-btn' onClick={() => setIsOpenCreateEditTask(true)}>
+        <img src={plusIcon} alt="Plus Icon" />
+        New Task
+      </div>
     </section>
   )
 }
