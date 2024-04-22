4005d25326f50dd0f407aef6357672cdcccccba3
zulfiqarshafar <zul.shafar@gmail.com>
2024-04-23 05:33:23 +0800

    feat: update task content design

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -4,13 +4,20 @@ import plusIcon from '../assets/images/plus-circle.svg'
 import '../assets/css/Column.css'
 
 function Column({ columnClass, columnTitle, columnMonth, setIsOpenCreateEditTask }) {
+
   return (
     <section className={`column ${columnClass}`}>
       <div className='column-title'>{ columnTitle }</div>
       <div className='column-month'>{ columnMonth }</div>
-      <Task />
+
+      <Task title='Data Migration: Performance & Culture End Game' progress='100' />
+
+      <section className='task no-task'>
+        <div className="task-title no-task-title">No Task</div>
+      </section>
+
       <div className='new-task-btn' onClick={() => setIsOpenCreateEditTask(true)}>
-        <img src={plusIcon} alt="Plus Icon" />
+        <img src={plusIcon} alt="Plus icon" />
         New Task
       </div>
     </section>
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import moreIcon from '../assets/images/more-horizontal.svg'
+import checkIcon from '../assets/images/checklist.svg'
 import '../assets/css/Task.css'
 
-function Task() {
+function Task({ title, progress }) {
   return (
     <section className='task'>
-      <div className="task-title">Bundle improvement</div>
-      <div>Loader</div>
+      <div className="task-title">{ title }</div>
+      <div className="line-divider"></div>
+      <section className='task-description'>
+        <div className='progress'>
+          <div className="progress-bar">
+            <span className={`progress-bar-fill ${ progress == 100 && 'progress-bar-full'}`} style={{ width: `${progress}%`}} ></span>
+          </div>
+          <div className='progress-value'>
+            {
+              progress < 100 ? `${progress} %` : (<img src={checkIcon} alt="Check icon" />)
+            }
+          </div>
+        </div>
+        <div className='action-btn'>
+          <img src={moreIcon} alt="More icon" />
+        </div>
+      </section>
     </section>
 
   )
